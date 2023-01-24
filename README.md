ddd658d14db41503c3e5a1874d71168df6ceec97
xBrandonMorales <122809992+xBrandonMoralesx@users.noreply.github.com>
2023-01-24 19:43:23 +0000

    Create programa1.py

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 |No.1|Archivo|Descripcion|
 |--|--|--|
 |1.|peograma0.py|Hola mundo en python|
+|2.|programa1.py|Comentarios multilinea, Comentarios de una linea, Concatenacion de cadenas y cadena de caracteres|
---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""
+Programa1
+Nombre: Brandon CM
+Fecha: 23/01/2023
+Descripcion: Comentarios multilinea, Comentarios de una linea, Concatenacion de cadenas y cadena de caracteres
+"""
+variable1="Hola" #Variable que almacena una cadema de caracteres
+variable2=619 #Variable que almacena una cadena de caracteres
+print(variable1 + variable2) #Concatema 2 caracteres
