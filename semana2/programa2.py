c49faa6f9e629ac219b7f7f893b2df2b80657f2c
xBrandonMorales <122809992+xBrandonMoralesx@users.noreply.github.com>
2023-01-24 19:57:37 +0000

    CREATE programa2.py

---
+++ semana2/programa2.py
@@ -0,0 +1,9 @@
+"""
+Programa2
+Nombre: Brandon CM
+Fecha: 24/01/2023
+Descripcion: Comentarios multilinea, Comentarios de una linea, Concatenacion de cadenas y cadena de caracteres
+"""
+variable1="hola "
+variable2=619
+print("{}{}".format(variable1, variable2)) #El fortmat permite imprimir valores sin importar el tipo de datos
