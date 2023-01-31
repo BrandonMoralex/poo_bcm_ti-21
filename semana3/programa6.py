c8f9608763ddf8fc616d34075021d7ce5f67f8d2
xBrandonMorales <122809992+xBrandonMoralesx@users.noreply.github.com>
2023-01-31 19:35:45 +0000

    CREATE programa6.py

---
+++ semana3/programa6.py
@@ -0,0 +1,15 @@
+"""
+Programa6
+Nombre: Brandon CM
+Fecha: 31/01/2023
+Descripcion: Comentarios multilinea, Comentarios de una linea, Concatenacion de cadenas y cadena de caracteres
+"""
+a= float(input("tamaño del lado:"))
+b= float(input("tamaño de la base:"))
+h= float(input("tamaño de la altura:"))
+
+area= (b*h)/2
+perimetro = 3*a
+
+print("El área es: ", area)
+print("El perímetro es: ", perimetro)
