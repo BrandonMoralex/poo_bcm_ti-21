6fd778ab6c8c0b61a9d4287c40723a380242a987
xBrandonMorales <122809992+xBrandonMoralesx@users.noreply.github.com>
2023-01-31 20:44:46 +0000

    CREATE programa7.py

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -4,12 +4,10 @@ Nombre: Brandon CM
 Fecha: 31/01/2023
 Descripcion: Comentarios multilinea, Comentarios de una linea, Concatenacion de cadenas y cadena de caracteres
 """
-a= float(input("tamaño del lado:"))
-b= float(input("tamaño de la base:"))
-h= float(input("tamaño de la altura:"))
-
+a= float(input("tamaño del lado:")) #Da un dato al lado
+b= float(input("tamaño de la base:")) #Da datos al valor
+h= float(input("tamaño de la altura:")) #Da datos al valor
 area= (b*h)/2
 perimetro = 3*a
-
-print("El área es: ", area)
-print("El perímetro es: ", perimetro)
+print("El área es: ", area) #Da el dato del area
+print("El perímetro es: ", perimetro) #Da el dato del perimetro
---
+++ semana3/programa7.py
@@ -0,0 +1,25 @@
+"""
+Programa7
+Nombre: Brandon CM
+Fecha: 31/01/2023
+Descripcion: Comentarios multilinea, Comentarios de una linea, Concatenacion de cadenas y cadena de caracteres
+"""
+print ("Area y perimetro de un circulo") #imprime el area la cual se saca asi El área de un círculo es pi multiplicado por el radio al cuadrado (A = π r²). 
+diametro= float(input("ingrese el diametro de tu circulo:")) #Meta el dato del diametro
+radio= diametro/2
+area= (3.1416)*(radio)*(radio)
+perimetro = (2)*(3.1416)*(radio)
+print("El área del circulo de {} su radio es {}".format(diametro,area))# Ingrese el area, despues del paso anterior
+print("El perímetro de un circulo {} su radio es {}".format(diametro,perimetro))#ingrese el dato del perimetro
+
+
+
+print("") #Este dato me lo dieron mis amigos y es para dar un espacio
+
+
+print ("Area y perimetro de un cuadrado")#imprime lo que haremos
+lado= float(input("ingrese los lados del cuadrado:")) #da la orden para hacerlo
+area= lado*lado
+perimetro= lado+lado+lado+lado
+print("El area de un cuadrado es {}".format(lado*lado))#da el resultado del dato que pusiste
+print("el perimetro de un cuadrado es {}".format(lado+lado+lado+lado))#da el dato del perimetro
