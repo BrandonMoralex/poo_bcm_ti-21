f15ccdb1a99a505ba76768ec64f5386d01c43c85
xBrandonMorales <122809992+xBrandonMoralesx@users.noreply.github.com>
2023-02-07 20:19:58 +0000

    CREATE programa9.py

---
+++ semana3/programa9.py
@@ -0,0 +1,29 @@
+"""
+Programa9
+Nombre: Brandon CM
+Fecha: 2/02/2023
+Descripcion:Comparacion de dos numeros y impresion de los mismos.
+"""
+
+
+numero1=int(input("Ponga un numero: "))
+numero2=int(input("Ponga un numero: "))
+"""
+if numero1 == numero2:
+    print("none")
+
+elif numero1 > numero2: 
+    print("el numero es mayor que: ",numero1)
+
+elif numero2 > numero1 :
+    print("el numero mayor es: ",numero2)
+"""
+    
+if numero1 == numero2:
+    print("none")
+
+elif numero2 <= numero1:
+    print("el numero mayor es:  ",numero1)
+
+elif numero1 <= numero2:
+    print("el numero mayor es:  ",numero2)
