66c428ecdb09c29bbf3f546715cbb11cffde526c
xBrandonMorales <122809992+xBrandonMoralesx@users.noreply.github.com>
2023-03-21 00:05:39 +0000

    x

--- semana5/excel.py
+++ semana5/excel.py
@@ -1,11 +1,24 @@
-import openpyxl
-wb = openpyxl.Workbook()
+import csv
 
-import openpyxl
-wb = openpyxl.Workbook()
-hoja = wb.active
-print(f'Hoja activa: {hoja.title}')
-Hoja activa: Productos
-hoja.title = "Productos"
-print(f'Hoja activa: {wb.active.title}')
-Hoja activa: Productos
+class Tienda:
+    def __init__(self):
+        self.inventario = {
+            "001": {"nombre": "manzana", "precio": 0.5, "cantidad": 10},
+            "002": {"nombre": "banana", "precio": 0.3, "cantidad": 15},
+            "003": {"nombre": "naranja", "precio": 0.4, "cantidad": 20},
+            "004": {"nombre": "pera", "precio": 0.6, "cantidad": 12}
+        }
+
+    def actualizarTienda(self, archivo_csv: str) -> bool:
+        with open(archivo_csv, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            next(reader) # Salta la primera fila que contiene los encabezados del archivo CSV
+            for row in reader:
+                sku, nombre, precio, cantidad = row
+                if sku in self.inventario:
+                    self.inventario[sku]["nombre"] = nombre
+                    self.inventario[sku]["precio"] = float(precio)
+                    self.inventario[sku]["cantidad"] = int(cantidad)
+                else:
+                    self.inventario[sku] = {"nombre": nombre, "precio": float(precio), "cantidad": int(cantidad)}
+        return True
